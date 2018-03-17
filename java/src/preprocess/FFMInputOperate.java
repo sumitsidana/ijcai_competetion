2936f05a4f3c1a1e810e914b41c81150112e8a49
sumitsidana <sidana.sumit@gmail.com>
2018-03-18 00:44:23 +0100

    ffm input creation

--- java/src/preprocess/FFMInputOperate.java
+++ java/src/preprocess/FFMInputOperate.java
@@ -5,6 +5,6 @@ import java.io.IOException;
 public class FFMInputOperate {
 
 	public static void main(String [] args) throws IOException{
-		WriteFFMInput.writeFFMInput(args[0], args[1], args[2], args[3]);
+		FFMInputBeta.writeFFMInput(args[0], args[1], args[2], args[3]);	
 	}
 }
