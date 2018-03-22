2f74e6c482942e7e517c71ddd31a9012fa82d016
sidana <sidana@tiger.imag.fr>
2018-03-22 23:56:11 +0100

    ffm

--- ffm/runffm.sh
+++ ffm/runffm.sh
@@ -1,1 +1,3 @@
-./ffm-train -p /data/sidana/ijcai_competetion/baselines/ffm/test.csv.libffm -s 16 -l 0.001 -k 8 -r 0.2 --auto-stop /data/sidana/ijcai_competetion/baselines/ffm/train.csv.libffm  /data/sidana/ijcai_competetion/baselines/ffm/model
+./ffm-train -p /data/sidana/ijcai_competetion/baselines/ffm/test_va.csv.libffm -s 16 -l 0.001 -k 8 -r 0.2 -t 1000 --auto-stop /data/sidana/ijcai_competetion/baselines/ffm/train_va.csv.libffm  /data/sidana/ijcai_competetion/baselines/ffm/model
+./ffm-train  -s 16 -l 0.001 -k 8 -r 0.2 -t 174 /data/sidana/ijcai_competetion/baselines/ffm/train.csv.libffm  /data/sidana/ijcai_competetion/baselines/ffm/model
+./ffm-predict /data/sidana/ijcai_competetion/baselines/ffm/test.csv.libffm /data/sidana/ijcai_competetion/baselines/ffm/model /data/sidana/ijcai_competetion/baselines/ffm/output
