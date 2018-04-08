adedd6b98fd569e25c8c53aa47b747249a5298dd
sumitsidana <sidana.sumit@gmail.com>
2018-04-08 16:02:41 +0200

    saved file

--- java/src/preprocess/FFMInputOperate.java
+++ java/src/preprocess/FFMInputOperate.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class FFMInputOperate {
 
 	public static void main(String [] args) throws IOException{
-//		FFMInputBeta.writeFFMInput(args[0], args[1], args[2], args[3]);	
+		//		FFMInputBeta.writeFFMInput(args[0], args[1], args[2], args[3]);	
 		FFMInputStringFields.writeFFMInput(args[0], args[1], args[2], args[3]);
 	}
 }
